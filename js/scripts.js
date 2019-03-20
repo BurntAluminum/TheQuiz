ab9001e2c0f3b56f279b2193c1fd59f09832b1d1
Max Osborne <BurntAluminum@gmail.com>
2019-03-20 14:48:36 -0700

    This commit cleans up and adds some functional javascript

---
+++ js/scripts.js
@@ -0,0 +1,127 @@
+
+var cplusplus=0;
+var java=0;
+var ruby=0;
+
+// function add (number) {
+//   return number + 1;
+// }
+
+$(document).ready(function() {
+
+  $("#codequiz").submit(function(event) {
+    event.preventDefault();
+    var Q1= $("input:radio[name=Q1]:checked").val();
+    var Q2= $("input:radio[name=Q2]:checked").val();
+    var Q3= $("input:radio[name=Q3]:checked").val();
+    var Q4= $("input:radio[name=Q4]:checked").val();
+    var Q5= $("input:radio[name=Q5]:checked").val();
+
+  console.log(Q1+" "+Q2+" "+Q3+" "+Q4+" "+Q5)
+
+  if(condition)
+  {
+
+  }
+  else if(condition)
+})
+})
+// delete later, sucky guesswork that still explains how each answer should tally up scores
+//  $(".1-A").(function() {
+//    cplusplus = add (cplusplus);
+//  });
+//
+//  $(".1-B").(function() {
+//    cplusplus = add (cplusplus);
+//    ruby = add (ruby);
+//  });
+//
+//  $(".1-C").(function() {
+//    cplusplus = add(cplusplus);
+//    ruby = add(java);
+//  });
+//
+//  $(".1-D").(function() {
+//    ruby = add(ruby);
+//    cplusplus = add(cplusplus);
+//  });
+//
+//  $(".2-A").(function() {
+//    ruby = add(ruby);
+//    java = add(java);
+//  });
+//
+//  $(".2-B").(function() {
+//    cplusplus = add(cplusplus);
+//    java = add(java);
+//  });
+//
+//  $(".2-C").(function() {
+//    ruby = add(ruby);
+//    java = add(java);
+//  });
+//
+//  $(".2-D").(function() {
+//    java = add(java);
+//  });
+//
+//  $(".3-A").(function() {
+//    java = add(java);
+//    ruby = add(ruby);
+//  });
+//
+//  $(".3-B").(function() {
+//    cplusplus = add(cplusplus);
+//    ruby = add(ruby);
+//  });
+//
+//  $(".3-C").(function() {
+//    cplusplus = add(cplusplus);
+//    ruby = add(ruby);
+//    java = add(java);
+//  });
+//
+//  $(".4-A").(function() {
+//    cplusplus = add(cplusplus);
+//    ruby = add(ruby);
+//    java = add(java);
+//  });
+//
+//  $(".4-B").(function() {
+//    cplusplus = add(cplusplus);
+//    ruby = add(ruby);
+//    java = add(java);
+//  });
+//
+//  $(".4-C").(function() {
+//    cplusplus = add(cplusplus);
+//    ruby = add(ruby);
+//    java = add(java);
+//  });
+//
+//  $(".4-D").(function() {
+//    cplusplus = add(cplusplus);
+//    ruby = add(ruby);
+//    java = add(java);
+//  });
+//
+//  $(".5-A").(function() {
+//    java = add(java);
+//  });
+//
+//  $(".5-B").(function() {
+//    cplusplus = add(cplusplus);
+//    java = add(java);
+//  });
+//
+//  $(".5-C").(function() {
+//    cplusplus = add(cplusplus);
+//    ruby = add(ruby);
+//    java = add(java);
+//  });
+//
+//  $(".5-D").(function() {
+//    ruby = add(ruby);
+//    cplusplus = add(cplusplus);
+//    java = add(java);
+//  });
