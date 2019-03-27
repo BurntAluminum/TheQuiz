69ffe4cd5da54a6416bcfda780c120baf8a1871f
Max Osborne <BurntAluminum@gmail.com>
2019-03-27 15:27:11 -0700

    This commit brings the project to a near finished state and makes the quiz functional, the only thing missing is a readme.

--- js/scripts.js
+++ js/scripts.js
@@ -1,127 +1,133 @@
 
-var cplusplus=0;
+var c=0;
 var java=0;
 var ruby=0;
-
-// function add (number) {
-//   return number + 1;
-// }
+var php=0;
+var design=0;
 
 $(document).ready(function() {
-
-  $("#codequiz").submit(function(event) {
+  $("form#codequiz").submit(function(event) {
     event.preventDefault();
     var Q1= $("input:radio[name=Q1]:checked").val();
     var Q2= $("input:radio[name=Q2]:checked").val();
     var Q3= $("input:radio[name=Q3]:checked").val();
     var Q4= $("input:radio[name=Q4]:checked").val();
     var Q5= $("input:radio[name=Q5]:checked").val();
-
   console.log(Q1+" "+Q2+" "+Q3+" "+Q4+" "+Q5)
+  $(".resultc").hide();
+  $(".resultdesign").hide();
+  $(".resultjava").hide();
+  $(".resultruby").hide();
+  $("resultphp").hide();
 
-  if(condition)
-  {
-
+  if(Q1==="1") {
+    c+=2;
+  }else if(Q1==="2") {
+    c+=2;
+    ruby+=1;
+    php+=1;
+  }else if(Q1==="3") {
+    c+=1;
+    ruby+=1;
+  }else if(Q1==="4") {
+    c+=1;
+    php+=2;
+    design+=1;
   }
-  else if(condition)
+
+  if(Q2==="1") {
+    design+=2;
+    java+=1;
+  }else if(Q2==="2") {
+    c+=2;
+    java+=1;
+    ruby+=1;
+  }else if(Q2==="3") {
+    ruby+=2;
+    php+=1;
+  }else if(Q2==="4") {
+    java+=2;
+  }
+
+  if(Q3==="1") {
+    design+=1;
+    java+=2;
+    ruby+=2;
+  }else if(Q3==="2") {
+    c+=2;
+    java+=2;
+    php+=1;
+    ruby+=1;
+  }else if(Q3==="3") {
+    c+=1;
+    design+=1;
+    java+=1;
+    php+=1;
+    ruby+1;
+  }
+
+  if(Q4==="1") {
+  }
+  else if (Q4==="2") {
+  }
+  else if (Q4==="3") {
+  }
+  else if (Q4==="4") {
+    c+=1;
+    design+=1;
+    java+=1;
+    php+=1;
+    ruby+=1;
+  }
+
+  if (Q5==="1") {
+    design+=2;
+    java+=2;
+    php+=1;
+    ruby+=1;
+  }
+  else if (Q5==="2") {
+    c+=2;
+    java+=1;
+    ruby+=1;
+  }
+  else if (Q5==="3") {
+    java+=2;
+  }
+  else if (Q5==="4") {
+    php+=2;
+    ruby+=2;
+    c+=1;
+  }
+  console.log(design+" "+ c +" "+java+" "+php+" "+ruby)
+
+  if((c >= design) && (c >= java) && (c >= ruby) && (c >= php)) {
+    $(".resultc").show();
+  }
+
+  else if((design >= c) && (design >= java) && (c >= ruby) && (design >= php)) {
+    $(".resultdesign").show();
+  }
+
+  else if((java >= design) && (java >= c) && (java >= ruby) && (java >= php)) {
+    $(".resultjava").show();
+  }
+
+  else if ((ruby >= design) && (ruby >= c) && (ruby >= java) && (ruby >= php)) {
+    $(".resultruby").toggle();
+  }
+
+  else if ((php >= design) && (php >= c) && (php >= java) && (php >= ruby)) {
+    $(".resultphp").show();
+  }
+  c= 0;
+  java=0;
+  design=0;
+  php=0;
+  ruby=0;
+
+
+
 })
+
 })
-// delete later, sucky guesswork that still explains how each answer should tally up scores
-//  $(".1-A").(function() {
-//    cplusplus = add (cplusplus);
-//  });
-//
-//  $(".1-B").(function() {
-//    cplusplus = add (cplusplus);
-//    ruby = add (ruby);
-//  });
-//
-//  $(".1-C").(function() {
-//    cplusplus = add(cplusplus);
-//    ruby = add(java);
-//  });
-//
-//  $(".1-D").(function() {
-//    ruby = add(ruby);
-//    cplusplus = add(cplusplus);
-//  });
-//
-//  $(".2-A").(function() {
-//    ruby = add(ruby);
-//    java = add(java);
-//  });
-//
-//  $(".2-B").(function() {
-//    cplusplus = add(cplusplus);
-//    java = add(java);
-//  });
-//
-//  $(".2-C").(function() {
-//    ruby = add(ruby);
-//    java = add(java);
-//  });
-//
-//  $(".2-D").(function() {
-//    java = add(java);
-//  });
-//
-//  $(".3-A").(function() {
-//    java = add(java);
-//    ruby = add(ruby);
-//  });
-//
-//  $(".3-B").(function() {
-//    cplusplus = add(cplusplus);
-//    ruby = add(ruby);
-//  });
-//
-//  $(".3-C").(function() {
-//    cplusplus = add(cplusplus);
-//    ruby = add(ruby);
-//    java = add(java);
-//  });
-//
-//  $(".4-A").(function() {
-//    cplusplus = add(cplusplus);
-//    ruby = add(ruby);
-//    java = add(java);
-//  });
-//
-//  $(".4-B").(function() {
-//    cplusplus = add(cplusplus);
-//    ruby = add(ruby);
-//    java = add(java);
-//  });
-//
-//  $(".4-C").(function() {
-//    cplusplus = add(cplusplus);
-//    ruby = add(ruby);
-//    java = add(java);
-//  });
-//
-//  $(".4-D").(function() {
-//    cplusplus = add(cplusplus);
-//    ruby = add(ruby);
-//    java = add(java);
-//  });
-//
-//  $(".5-A").(function() {
-//    java = add(java);
-//  });
-//
-//  $(".5-B").(function() {
-//    cplusplus = add(cplusplus);
-//    java = add(java);
-//  });
-//
-//  $(".5-C").(function() {
-//    cplusplus = add(cplusplus);
-//    ruby = add(ruby);
-//    java = add(java);
-//  });
-//
-//  $(".5-D").(function() {
-//    ruby = add(ruby);
-//    cplusplus = add(cplusplus);
-//    java = add(java);
-//  });
